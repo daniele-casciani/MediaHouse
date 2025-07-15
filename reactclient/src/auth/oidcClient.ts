04b61ae90a89a1c9e4e300fce7b9343be78354d8
Daniele Casciani <10638165@polimi.it>
2025-07-15 10:23:00 +0000

    fixed paths

--- reactclient/src/auth/oidcClient.ts
+++ reactclient/src/auth/oidcClient.ts
@@ -6,7 +6,7 @@ const settings = {
     authority: process.env.REACT_APP_AUTH_ISSUER! || 'https://localhost:5006',
     client_id: process.env.REACT_APP_AUTH_CLIENT_ID! || '328404667920875523',
     redirect_uri: process.env.REACT_APP_AUTH_REDIRECT_URI! || 'https://localhost:5006/callback',
-    post_logout_redirect_uri: process.env.REACT_APP_AUTH_LOGOUT_REDIRECT_URI! || 'https://localhost:5006/',
+    post_logout_redirect_uri: process.env.REACT_APP_AUTH_LOGOUT_REDIRECT_URI! || 'https://localhost:5006/logout/callback',
     response_type: 'code',
     scope: 'openid profile email offline_access',
     userStore: new WebStorageStateStore({ store: window.localStorage }),
